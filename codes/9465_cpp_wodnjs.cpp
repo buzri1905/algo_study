e7b9f075c826c5b3b0b994bec14d79776f169d7e
resource777 <79133019+resource777@users.noreply.github.com>
2021-08-16 22:53:24 +0900

    Create 9465_cpp_wodnjs.cpp (#1)
    * Create 9465_cpp_wodnjs.cpp
    
    * Update 9465_cpp_wodnjs.cpp
    
    * Create 9465_changed_cpp_wodnjs.cpp
    
    9465 수정했습니다.

---
+++ codes/9465_cpp_wodnjs.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int score[100000][2];
+int res[3][3];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int t_case;
+	cin >> t_case;
+	for (int i = 0; i < t_case; i++)
+	{
+		int col;
+		cin >> col;
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < col; k++)
+			{
+				cin >> score[k][j];
+			}
+		}
+		res[0][0] = 0;//둘다 빈상태
+		res[0][1] = score[0][0];//위에거
+		res[0][2] = score[0][1];//아래거
+		res[1][0] = max(res[0][1], res[0][2]);
+		res[1][1] = res[0][2] + score[1][0];
+		res[1][2] = res[0][1] + score[1][1];
+		for (int i = 2; i < col; i++)
+		{
+			res[2][0] = max(max(res[1][0], res[1][1]), res[1][2]);
+			res[2][1] = max(res[1][2], res[1][0]) + score[i][0];
+			res[2][2] = max(res[1][1], res[1][0]) + score[i][1];
+			for (int j = 0; j < 3; j++)
+			{
+				res[0][j] = res[1][j];
+				res[1][j] = res[2][j];
+			}
+		}
+		cout << max(max(res[1][0], res[1][1]), res[1][2])<<'\n';
+	}
+	return 0;
+}
