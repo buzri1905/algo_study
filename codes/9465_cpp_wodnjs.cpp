121b06f3d3f80b2b4da11c9e5acefae697499568
buzri1905 <buzri1905@naver.com>
2021-08-16 22:57:01 +0900

    Fix merge error

--- codes/9465_cpp_wodnjs.cpp
+++ codes/9465_cpp_wodnjs.cpp
@@ -2,14 +2,14 @@
 #include<algorithm>
 using namespace std;
 int score[100000][2];
-int res[3][3];
+int res[2][3];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 	int t_case;
 	cin >> t_case;
-	for (int i = 0; i < t_case; i++)
+	for (int t = 0; t < t_case; t++)
 	{
 		int col;
 		cin >> col;
@@ -20,21 +20,17 @@ int main() {
 				cin >> score[k][j];
 			}
 		}
-		res[0][0] = 0;//둘다 빈상태
-		res[0][1] = score[0][0];//위에거
-		res[0][2] = score[0][1];//아래거
-		res[1][0] = max(res[0][1], res[0][2]);
-		res[1][1] = res[0][2] + score[1][0];
-		res[1][2] = res[0][1] + score[1][1];
-		for (int i = 2; i < col; i++)
+		res[1][0] = 0;//둘다 빈상태
+		res[1][1] = score[0][0];//위에거
+		res[1][2] = score[0][1];//아래거
+		for (int i = 1; i < col; i++)
 		{
-			res[2][0] = max(max(res[1][0], res[1][1]), res[1][2]);
-			res[2][1] = max(res[1][2], res[1][0]) + score[i][0];
-			res[2][2] = max(res[1][1], res[1][0]) + score[i][1];
+			res[0][0] = max(max(res[1][0], res[1][1]), res[1][2]);
+			res[0][1] = max(res[1][2], res[1][0]) + score[i][0];
+			res[0][2] = max(res[1][1], res[1][0]) + score[i][1];
 			for (int j = 0; j < 3; j++)
 			{
-				res[0][j] = res[1][j];
-				res[1][j] = res[2][j];
+				res[1][j] = res[0][j];
 			}
 		}
 		cout << max(max(res[1][0], res[1][1]), res[1][2])<<'\n';
