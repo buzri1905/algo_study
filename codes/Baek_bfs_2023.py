cc259e28effcd32864d4edadf25e71f7ad842b0d
JunTae-Kim-code <70574529+JunTae-Kim-code@users.noreply.github.com>
2021-08-23 03:07:55 +0900

    Add files via upload

---
+++ codes/Baek_bfs_2023.py
@@ -0,0 +1,22 @@
+import sys
+import collections
+input = sys.stdin.readline
+
+N=int(input())
+prime_list = [2,3,5,7]
+q = collections.deque([2, 3, 5, 7])
+while q:
+    cur_prime = str(q.popleft())
+    if len(cur_prime)==N:
+        print(int(cur_prime))
+        continue
+    for next_num in range(1, 10, 2):
+        next_total = int(cur_prime+str(next_num))
+        division=0
+        for divider in range(2, int(next_total**0.5)+1):
+            if next_total%divider==0:
+                division=1
+                break
+        if division:
+            continue
+        q.append(next_total)
