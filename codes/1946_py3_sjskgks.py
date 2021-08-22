dd9ff38ef69fc4588799d7a45a3207731d33bc44
JunTae-Kim-code <70574529+JunTae-Kim-code@users.noreply.github.com>
2021-08-22 17:31:48 +0900

    Update 1946_py3_sjskgks.py

--- codes/1946_py3_sjskgks.py
+++ codes/1946_py3_sjskgks.py
@@ -3,13 +3,12 @@ input = sys.stdin.readline
 
 for case in range(int(input())):
     numOfapp = int(input())
-    applicants = [list(map(int, input().split())) for _ in range(numOfapp)]
-    applicants.sort()
+    applicants = sorted([list(map(int, input().split())) for _ in range(numOfapp)])
     recruit = 1
     limit = applicants[0][1]
-    for i in range(1, numOfapp):
-        if applicants[i][1]<limit:
-            limit = min(limit, applicants[i][1])
+    for _, rank in applicants:
+        if rank<limit:
+            limit = rank
             recruit+=1
         if limit == 1:
             break
