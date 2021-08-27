a9963c0e6cbfbe4b29f7ae0298f12cebf7800f8c
sjskgks <sjskgks@korea.ac.kr>
2021-08-27 20:49:34 +0900

    modified 1707_py3_sjskgks.py

--- codes/1707_py3_sjskgks.py
+++ codes/1707_py3_sjskgks.py
@@ -6,7 +6,7 @@ numOfCase = int(input())
 for _ in range(numOfCase):
     numOfVertex, numOfEdge = map(int, input().split())
     graph=[[] for _ in range(numOfVertex+1)]
-    for __ in range(numOfEdge):
+    for _ in range(numOfEdge):
         start, dest = map(int, input().split())
         graph[start].append(dest)
         graph[dest].append(start)
@@ -18,13 +18,13 @@ for _ in range(numOfCase):
         q = deque([[vertex , 1]])
         while q:
             cur_pos, pos_flag = q.popleft()
-            if visit[cur_pos]==-1*pos_flag:
+            if visit[cur_pos]==-pos_flag:
                 is_biparatite=False
                 break
             if not visit[cur_pos]:
                 visit[cur_pos]=pos_flag
                 for next_pos in graph[cur_pos]:
-                    q.append([next_pos, -1*pos_flag])
+                    q.append([next_pos, -pos_flag])
         if not is_biparatite:
             break
     if is_biparatite:
