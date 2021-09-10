7a16a377dfc11a37e4ec9274fe6dfb0513557dcb
yslee96 <dbstn322@gmail.com>
2021-09-11 00:43:51 +0900

    Revert "1916번 문제풀이"
    This reverts commit d1cc9b334c1d9ee76bea020e25c26b7d2ab1b6c6.

--- codes/1965_cpp_dbstn322.cpp
+++ codes/1965_cpp_dbstn322.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 int dp[1001];
-int box[1001];
 
 int main()
 {
     int n;
     cin >> n;
 
-    int maximum = 0;
+    int box[n];
 
     for (int i = 0; i < n; i++)
-        cin >> box[i];
+    {
+        int num;
+        cin >> num;
+        box[i] = num;
+    }
+
+    int max = 0;
 
     for (int i = 0; i < n; i++)
     {
         dp[i] = 1;
         for (int j = 0; j < i; j++)
         {
-            if (box[j] < box[i])
-                dp[i] = max(dp[i], dp[j] + 1);
+            if (box[j] < box[i] && dp[j] >= dp[i])
+                dp[i] = dp[j] + 1;
         }
 
-        if (maximum < dp[i])
-            maximum = dp[i];
+        if (max < dp[i])
+            max = dp[i];
     }
 
-    cout << maximum << endl;
+    cout << max << endl;
 
     return 0;
 }
