29d039f04e3fb4eaf05b20f32115dab12327cfed
yslee96 <77106988+yslee96@users.noreply.github.com>
2021-09-15 00:22:13 +0900

    Create 1965_cpp_dbstn322.cpp (#26)
    * Create 1965_cpp_dbstn322.cpp
    
    * 1916번 문제풀이
    
    다익스트라
    
    * Revert "1916번 문제풀이"
    
    This reverts commit d1cc9b334c1d9ee76bea020e25c26b7d2ab1b6c6.
    
    * Create 1916_cpp_dbstn322.cpp
    
    * Revert "Create 1916_cpp_dbstn322.cpp"
    
    This reverts commit 63f67e7267f5a448775a70edc70cf161cb3a11c5.
    
    * 1965 modified

---
+++ codes/1965_cpp_dbstn322.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int dp[1001];
+int box[1001];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> box[i];
+
+    int maximum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (box[j] < box[i])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+
+        if (maximum < dp[i])
+            maximum = dp[i];
+    }
+
+    cout << maximum << endl;
+
+    return 0;
+}
