6239a0fd7a0fd2c391819c4a2acdf3c68223d66f
yslee96 <dbstn322@gmail.com>
2021-09-14 17:46:41 +0900

    1965 modified

--- codes/1965_cpp_dbstn322.cpp
+++ codes/1965_cpp_dbstn322.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 int dp[1001];
+int box[1001];
 
 int main()
 {
     int n;
     cin >> n;
 
-    int box[n];
-
     for (int i = 0; i < n; i++)
-    {
-        int num;
-        cin >> num;
-        box[i] = num;
-    }
+        cin >> box[i];
 
-    int max = 0;
+    int maximum = 0;
 
     for (int i = 0; i < n; i++)
     {
         dp[i] = 1;
         for (int j = 0; j < i; j++)
         {
-            if (box[j] < box[i] && dp[j] >= dp[i])
-                dp[i] = dp[j] + 1;
+            if (box[j] < box[i])
+                dp[i] = max(dp[i], dp[j] + 1);
         }
 
-        if (max < dp[i])
-            max = dp[i];
+        if (maximum < dp[i])
+            maximum = dp[i];
     }
 
-    cout << max << endl;
+    cout << maximum << endl;
 
     return 0;
 }
