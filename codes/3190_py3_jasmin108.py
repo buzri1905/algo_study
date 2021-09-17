25def64477cd8af3e32b8d16da7fcbdb7a453e0c
yun-ss97 <yunseung_lee@korea.ac.kr>
2021-09-17 21:15:35 +0900

    Revised code 3190 jasmin108

--- codes/3190_py3_jasmin108.py
+++ codes/3190_py3_jasmin108.py
@@ -1,58 +1,48 @@
-n = int(input())
-k = int(input())
-k_loc = [list(map(int, input().split())) for _ in range(k)]
-l = int(input())
-l_loc = []
-for _ in range(l):
-    x, c = input().split()
-    l_loc.append([int(x), c])
+from collections import deque
+N = int(input())
+mat = [[0]*N for _ in range(N)]
 
-mat = [[0]*n for _ in range(n)]
-
-for i,j in k_loc:
+for _ in range(int(input())):
+    i, j = map(int, input().split())
     mat[i-1][j-1] = 1
 
-pos = [(0,1), (0,-1), (-1,0), (1,0)] # 우, 좌, 상, 하
-cnt = 1
-x, y = 0,0
-sx, sy = pos[0]
+dir_dict = {}
+for _ in range(int(input())):
+    x, c = input().split()
+    dir_dict[int(x)] = c
 
-def check_pos(sx, sy):
-    if (sx,sy) == (0,1):
-        return (-1,0), (1,0)
-    elif (sx,sy) == (0,-1):
-        return (1,0), (-1,0)
-    elif (sx,sy) == (1,0):
-        return (0,1), (0,-1)
-    else:
-        return (0,-1), (0,1)
+pos = [(0,-1), (1,0), (0,1), (-1,0)] # 좌, 하, 우, 상
 
-snake = [[0,0]]
-while 0 <= x+sx < n and 0 <= y+sy < n:
-    if snake[0][0] == snake[-1][0] + sx and snake[0][1] == snake[-1][1] + sy:
+cur_dir = 2 # 우
+x,y = 0, 0
+
+snake = deque([[0, 0]])
+
+cnt = 0
+while True:
+    curr_pos = pos[cur_dir]
+    x += curr_pos[0]
+    y += curr_pos[1]
+
+    if not(0 <= x < N and 0 <= y < N):
         break
 
-    for i, j in l_loc:
-        if i == cnt:
-            l, r = check_pos(sx, sy)
-            if j == 'L':
-                sx, sy = l
-            else:
-                sx, sy = r
-            l_loc.pop(0)
+    if [x,y] in snake:
+        break
 
-    if mat[x+sx][y+sy] == 1: # 사과가 있다면
-        snake.append([x+sx, y+sy])
-        mat[x+sx][y+sy] = 2
-        cnt += 2
+    if mat[x][y] == 0:
+        del_x, del_y = snake.popleft()
+        mat[del_x][del_y] = 0
+    mat[x][y] = 2
+    snake.append([x, y])
+    cnt += 1
 
-    else: # 사과가 없다면
-        tx, ty = snake.pop(0)
-        mat[tx][ty] = 0
-        snake.append([x+sx, y+sy])
-        mat[x+sx][y+sy] = 2
-        cnt += 1
+    if cnt in dir_dict.keys():
+        new_dir = dir_dict[cnt]
+        if new_dir == 'L':
+            cur_dir = (cur_dir+1)%4
+        else:
+            cur_dir = (cur_dir+3)%4
+        curr_pos = pos[cur_dir]
 
-    x += sx
-    y += sy
-print(cnt)
+print(cnt+1)
