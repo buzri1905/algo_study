9d502b11d255f576adf465480529d658ce8bdc51
sjskgks <sjskgks@korea.ac.kr>
2021-09-25 01:43:21 +0900

    10775_py3_sjskgks.py is modified

--- codes/10775_py3_sjskgks.py
+++ codes/10775_py3_sjskgks.py
@@ -2,22 +2,21 @@ import sys
 input = sys.stdin.readline
 
 numOfGate = int(input())
-numOfPlane = int (input())
+numOfPlane = int(input())
 possibleDock = [i for i in range(numOfGate+1)]
+
+def find_gate(try_dock):
+    global possibleDock
+    if possibleDock[try_dock]==try_dock:
+        possibleDock[try_dock]=possibleDock[try_dock-1]
+        return try_dock
+    possibleDock[try_dock] = find_gate(possibleDock[try_dock])
+    return possibleDock[try_dock]
+
 numOfDock = 0
-is_full = False
 for _ in range(numOfPlane):
     try_dock=int(input())
-    if is_full:
-        continue
-    original_try = try_dock
-    while try_dock:
-        if possibleDock[try_dock]==try_dock:
-            numOfDock+=1
-            possibleDock[try_dock]=possibleDock[possibleDock[try_dock-1]]
-            possibleDock[original_try]=possibleDock[try_dock]
-            break
-        try_dock=possibleDock[try_dock]
-    if try_dock==0:
-        is_full=True
+    if find_gate(try_dock)==0:
+        break
+    numOfDock+=1
 print(numOfDock)
