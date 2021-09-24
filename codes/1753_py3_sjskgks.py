dcaa593b37c65b5eb792ab2f3e32536be64dce04
sjskgks <sjskgks@korea.ac.kr>
2021-09-25 00:55:09 +0900

    1753_py3_sjskgks.py is modified

--- codes/1753_py3_sjskgks.py
+++ codes/1753_py3_sjskgks.py
@@ -4,23 +4,20 @@ input = sys.stdin.readline
 
 numOfVertex, numOfEdge = map(int, input().split())
 start_node = int(input())-1
-Graph = [[] for _ in range(numOfVertex)]
+graph = [[] for _ in range(numOfVertex)]
 for _ in range(numOfEdge):
     start, dest, cost = map(int, input().split())
-    Graph[start-1].append([cost, dest-1])
+    graph[start-1].append([cost, dest-1])
 
-visit = [float("inf") for _ in range(numOfVertex)]
+dist = ["INF" for _ in range(numOfVertex)]
 q = []
 heapq.heappush(q, [0, start_node])
 while q:
     cost, cur_node = heapq.heappop(q)
-    if visit[cur_node]!=float('inf'):
+    if dist[cur_node]!="INF":
         continue
-    visit[cur_node]=cost
-    for next_cost, next_node in Graph[cur_node]:
+    dist[cur_node]=cost
+    for next_cost, next_node in graph[cur_node]:
         heapq.heappush(q, [cost+next_cost, next_node])
 for vertex in range(numOfVertex):
-    if visit[vertex] == float("inf"):
-        print("INF")
-    else:
-        print(visit[vertex])
+        print(dist[vertex])
