29797491527483206fcb5499188c1aee3720e0f4
yslee96 <dbstn322@gmail.com>
2021-10-01 10:29:27 +0900

    1916_cpp_dbstn322 modified

--- codes/1916_cpp_dbstn322.cpp
+++ codes/1916_cpp_dbstn322.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-vector<pair<int, int>> edges[100001];
-priority_queue<pair<int, int>> shortDist;
+vector<pair<int, int> > edges[100001];
+priority_queue<pair<int, int> > shortDist;
 int dists[1001];
 
 int main(void)
@@ -36,7 +36,7 @@ int main(void)
         if (dists[node] != -1)
             continue;
         dists[node] = dist;
-        for (vector<pair<int, int>>::iterator iter = edges[node].begin(); iter != edges[node].end(); iter++)
+        for (vector<pair<int, int> >::iterator iter = edges[node].begin(); iter != edges[node].end(); iter++)
             if (dists[iter->first] == -1)
                 shortDist.push(make_pair(-(dist + iter->second), iter->first));
     }
